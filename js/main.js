5cd42f93f270212a559c586b15656c1a3bc2d9a3
unknown <alaric@UIT_RS_118.aviaagregat.local>
2017-05-12 13:39:45 +0400

    Защита 1

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ var link = document.querySelector(".buttons-contacts");
 var popup = document.querySelector(".popup");
 var close = document.querySelector(".popup-close");
 
-link.addEventLestener("click", function(event) {
+link.addEventListener("click", function(event) {
   event.preventDefault();
   popup.classList.add("popup-show");
 });
@@ -13,9 +13,9 @@ close.addEventListener("click", function(event) {
 });
 
 window.addEventListener("keydown", function(event) {
-  if (evt.keyCode == 27) {
+  if (event.keyCode == 27) {
     if (popup.classList.contains("popup-show")) {
       popup.classList.remove("popup-show");
     }
   }
-})
+});
